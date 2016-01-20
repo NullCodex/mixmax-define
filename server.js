70a4dae9f1fc1516ba724f4243cd01692287b6ab
NullCodex <jamesonyu95@gmail.com>
2016-01-19 21:28:58 -0500

    Revamped

--- server.js
+++ server.js
@@ -3,13 +3,25 @@ var app = express();
 var request = require('request');
 var port = process.env.PORT || 9000;
 
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', 'https://compose.mixmax.com');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
     res.header('Access-Control-Allow-Credentials', 'true');
 
     next();
 });
+
+function wordDefintion(word, callback) {
+    var apiUrl = 'https://montanaflynn-dictionary.p.mashape.com/define?word=' + term;
+    request({
+        url: apiUrl,
+        headers: {
+            'X-Mashape-Key': 'ICs9hz1mdMmshBp7lePPo5ehopQgp1zjmgJjsnbzXVNzkGuJWe'
+        },
+        json: true,
+        timeout: 10 * 1000
+    }, callback);
+}
 
 app.get('/definetypeahead', function(req, res) {
     var term = req.query.text.trim();
@@ -21,70 +33,42 @@ app.get('/definetypeahead', function(req, res) {
         return;
     }
 
-    var response;
-    try {
-        response = sync.await(request({
-            url: 'https://montanaflynn-dictionary.p.mashape.com/define?word=' + term,
-            headers: {
-                'X-Mashape-Key': 'ICs9hz1mdMmshBp7lePPo5ehopQgp1zjmgJjsnbzXVNzkGuJWe'
-            },
-            json: true,
-            timeout: 10 * 1000
-        }, sync.defer()));
-    } catch (e) {
-        res.status(500).send('Error');
-        return;
-    }
+    wordDefintion(term, function(err, response) {
+        var definition = response.body.definitions;
+        var results;
+        if (definition.length === 0) {
+            results = [{
+                title: '<i>(no results)</i>',
+                text: ''
+            }]
 
-    if (response.statusCode !== 200 || !response.body) {
-        res.status(500).send('Error');
-        return;
-    }
+        } else {
+            results = [{
+                title: '<i>' + definition[0].text + '<i>',
+                text: ''
+            }]
+        }
+        return res.json(results);
+    });
 
-    var definition = response.body.definitions;
-    var results;
-    if (definition.length === 0) {
-        results = [{
-            title: '<i>(no results)</i>',
-            text: ''
-        }]
 
-    } else {
-        results = [{
-            title: '<i>' + definition[0].text + '<i>',
-            text: ''
-        }]
-    }
-    res.json(results);
 });
 
 app.get('/defineresolver', function(req, res) {
     var term = req.query.text.trim();
-    var response;
-    try {
-        response = sync.await(request({
-            url: 'https://montanaflynn-dictionary.p.mashape.com/define?word=' + term,
-            headers: {
-                'X-Mashape-Key': 'ICs9hz1mdMmshBp7lePPo5ehopQgp1zjmgJjsnbzXVNzkGuJWe'
-            },
-            json: true,
-            timeout: 10 * 1000
-        }, sync.defer()));
-    } catch (e) {
-        res.status(500).send('Error');
-        return;
-    }
 
-    var definition = response.body.definitions;
-    var html;
-    if (definition.length === 0) {
-      html = '<i> No results </i>';
+    wordDefintion(term, function(err, response) {
+        var definition = response.body.definitions;
+        var html;
+        if (definition.length === 0) {
+            html = '<i> No results </i>';
+        } else {
+            html = '<i>' + definition[0].text + '</i>';
+        }
+        return res.json({
+            body: html
+        });
 
-    } else {
-      html = '<i>' + definition[0].text + '</i>';
-    }
-    res.json({
-        body: html
     });
 });
 
